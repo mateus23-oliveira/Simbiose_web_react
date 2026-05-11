6b69ff46b98bf8c07a6b86056ab81ef074726977
mateus23-oliveira <mateus17333@gmail.com>
2026-05-11 15:17:01 -0300

    mudando escopo da API para cadastro de animal com a finalidade de criar um banco de especies

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export class Database {
   static async connect() {
     try {
-      await mongoose.connect("mongodb://localhost:27017/api-users");
+      await mongoose.connect("mongodb://localhost:27017/api-especies");
       console.log("MongoDB conectado");
     } catch (error) {
       console.error("Erro ao conectar no MongoDB", error);
