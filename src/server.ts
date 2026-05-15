b43401bb9700ba609cf4451778e5d69f35c21680
jhonepa <eco.jhone@gmail.com>
2026-05-15 16:24:55 -0300

    instalação e configuração do CORS para permitir acesso do frontend

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
 import { Database } from "./config/database";
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 Database.connect();
